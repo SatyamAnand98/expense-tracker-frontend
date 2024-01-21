96630874840ae54a27ab8e55ffea684a30abedc6
Satyam Anand <satyam.anand@bolt.earth>
2024-01-22 04:12:54 +0530

    minor

--- src/store/configs.js
+++ src/store/configs.js
@@ -1,8 +1,13 @@
 const PORT = 3001;
+const URL = "https://expense-tracker-7r6c.onrender.com";
+
+// export const AppConfigs = {
+//     BACKEND_PORT: "3001",
+//     BACKEND_BASE_URL: "http://localhost:" + PORT,
+//     BACKEND_API_VERSION: "v1",
+//     BACKEND_API_URL: `http://localhost:${PORT}/api/v1`,
+// };
 
 export const AppConfigs = {
-    BACKEND_PORT: "3001",
-    BACKEND_BASE_URL: "http://localhost:" + PORT,
-    BACKEND_API_VERSION: "v1",
-    BACKEND_API_URL: `http://localhost:${PORT}/api/v1`,
+    BACKEND_API_URL: `${URL}/api/v1`,
 };
